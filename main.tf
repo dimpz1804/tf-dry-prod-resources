50d489a9d345761bda94609048c96618fb2979aa
dimpz1804 <dimpy.chugh@hashicorp.com>
2025-05-13 22:44:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,14 @@
-/*resource "null_resource" "example" {
-  provisioner "local-exec" {
-    command = "echo This command will execute only when the configuration changes"
-  }
- triggers = {
-  always_run = "${timestamp()}"
- }
-}*/
-
 resource "null_resource" "previous" {}
 
 
-resource "time_sleep" "wait_300_seconds" {
+resource "time_sleep" "wait_60_seconds" {
   depends_on = [null_resource.previous]
 
-  create_duration = "300s"
+  create_duration = "60s"
 
 }
 
 resource "null_resource" "next" {
 
-  depends_on = [time_sleep.wait_300_seconds]
+  depends_on = [time_sleep.wait_60_seconds]
 }
