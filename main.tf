f97ae496a00a4498e49b6aa804645a9ef776d69c
dimpz1804 <dimpy.chugh@hashicorp.com>
2025-05-13 22:47:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 resource "null_resource" "previous" {}
 
 
-resource "time_sleep" "wait_60_seconds" {
+resource "time_sleep" "wait_50_seconds" {
   depends_on = [null_resource.previous]
-  create_duration = "60s"
+  create_duration = "50s"
 
 }
 
 resource "null_resource" "next" {
 
-  depends_on = [time_sleep.wait_60_seconds]
+  depends_on = [time_sleep.wait_50_seconds]
 }
