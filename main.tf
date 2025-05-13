603b3e5bc1735fd4acd3fa054afb27dc45f48203
dimpz1804 <dimpy.chugh@hashicorp.com>
2025-05-13 22:26:03 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "echo This command will execute only when the configuration changes"
+  }
+ triggers = {
+  always_run = “${timestamp()}”
+ }
+}
