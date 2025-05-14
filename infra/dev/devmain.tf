03ebeff8f9ff6763986d48d44517067d7f6a599f
dimpz1804 <dimpy.chugh@hashicorp.com>
2025-05-14 09:05:15 +0530

    Create devmain.tf

---
+++ infra/dev/devmain.tf
@@ -0,0 +1,9 @@
+resource "null_resource" "example-main" {
+
+  triggers = {
+    always_run = timestamp()
+  }
+  provisioner "local-exec" {
+    command = "echo This specific command will execute every time during apply for the dev environment"
+  }
+}
